982228dcc06910994762e3e230f5af5b5436a112
Cao Cong <cong19999@gmail.com>
2023-06-15 19:35:39 +0700

    worldcup

--- insert_data.sh
+++ insert_data.sh
@@ -29,15 +29,9 @@ do
       $($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
       # OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE team=$OPPONENT");
     fi
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'");
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
+    $($PSQL "INSERT INTO games(winner_id, opponent_id, winner_goals, opponent_goals, year, round) VALUES ($WINNER_ID, $OPPONENT_ID,$WINNER_GOALS, $OPPONENT_GOALS, $YEAR, '$ROUND')")
 
-    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name=$WINNER");
-    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT' ");
-    $($PSQL "INSERT INTO games(winner_id, opponent_id, winner_goals, opponent_goals, year, round) VALUES ($WINNER_ID, $OPPONENT_ID,$WINNER_GOALS, $OPPONENT_GOALS, $YEAR, '$ROUND' )")
-    
   fi
-done
-
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-do
-
 done
